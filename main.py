84aaf99078d6d6f8e5122ef11113f9f3efefcb89
arnitkun <ankit.nitagartala@gmail.com>
2020-06-04 22:48:20 +0530

    intial data processing

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("pump_data.csv")
+
+#removing instances where ON/OFF status of pump is unknown
+data = data[data.BFPT_B_LP_SPEED_INPUT__1__ !=0]
+
+#finding % of data missing
+# print(data.isnull().mean()) 
+
+#dropping columns where more than 49% values are missing
+
+missing_thresh = len(data)*0.49
+data = data.dropna(thresh=missing_thresh, axis = 1)
+
+
+#splitting the filtered data into train and test sets
+train_set, test_set = train_test_split(data, test_size = 0.20, random_state=10)
+
+
+
+print(train_set.isnull().sum()) 
+# print(train_set.shape)
+# print(train_set.isnull().sum())
+# print(data.head(5))
