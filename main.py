f28f12188e777c2948260245f0c33181fdb0a045
arnitkun <ankit.nitagartala@gmail.com>
2020-06-05 22:28:44 +0530

    removed extra datetime data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sklearn.impute import SimpleImputer
 import numpy as np
 # from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("pump_data.csv")
+data = pd.read_csv("pump_data.csv", index_col=0)
 
 #removing instances where ON/OFF status of pump is unknown
 data = data[data.BFPT_B_LP_SPEED_INPUT__1__ !=0]
@@ -14,15 +14,17 @@ data = data.dropna(thresh=missing_thresh, axis = 1)
 
 #creating imputer that will place median where values are missing
 imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-imputer.fit(data.iloc[1:, 1:])
-data.iloc[1:, 1:] = imputer.transform(data.iloc[1:, 1:])
+imputer.fit(data.iloc[:, :])
+data.iloc[:, :] = imputer.transform(data.iloc[:, :])
 
+#dropping empty rows
+data.dropna()
 
 #finding % of data missing
 print(data.isnull().sum()) 
 print(data.shape)
 
-# data.to_csv('out.csv', index=False)
+data.to_csv('out.csv', index=False)
 
 #splitting the filtered data into train and test sets
 # train_set, test_set = train_test_split(data, test_size = 0.20, random_state=10)
